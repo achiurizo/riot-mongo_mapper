bedaa6ff6e0792b7ac9003130ee2b00394952aa6
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-07-04 00:41:18 -0700

    clean up has_key macro

--- test/has_key_test.rb
+++ test/has_key_test.rb
@@ -1,6 +1,7 @@
 require File.join(File.dirname(__FILE__),'teststrap')
 
 context "has_key macro" do
+  setup { @assertion = RiotMongoMapper::HasKeyAssertion.new }
   setup do
     mock_model do
       key :name, String
@@ -10,23 +11,24 @@ context "has_key macro" do
   end
 
   asserts "passes when the key is specified" do
-    RiotMongoMapper::HasKeyAssertion.new.evaluate(topic, :name, String).first
+    @assertion.evaluate(topic, :name, String).first
   end.equals(:pass)
   
   asserts "fails when the key doesn't match" do 
-    RiotMongoMapper::HasKeyAssertion.new.evaluate(topic, :name, Array).first
+    @assertion.evaluate(topic, :name, Array).first
   end.equals(:fail)
   
-  asserts "returns test message" do 
-    RiotMongoMapper::HasKeyAssertion.new.evaluate(topic, :name, String).last
-  end.matches %r{has key 'name' with options}
-
   asserts "passes when options are specified" do 
-    RiotMongoMapper::HasKeyAssertion.new.evaluate(topic, :age, Integer, :required => true, :default => 18).first
+    @assertion.evaluate(topic, :age, Integer, :required => true, :default => 18).first
   end.equals(:pass)
   
   asserts "fails when the options specifed don't match" do 
-    RiotMongoMapper::HasKeyAssertion.new.evaluate(topic, :foo, Boolean, :default => true).first
+    @assertion.evaluate(topic, :foo, Boolean, :default => true).first
   end.equals(:fail)
+
+  asserts "returns a message" do 
+    @assertion.evaluate(topic, :name, String).last
+  end.matches %r{has key 'name' with options}
+
 
 end
