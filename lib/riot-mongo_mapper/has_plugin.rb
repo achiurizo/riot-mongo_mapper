0af93df4617c95771d777707dc4ad7f06a0b6efb
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-07-23 11:37:59 -0700

    [has_plugin] dont really like this but in case its not testing the actual class, but an instance, look up into the class.

--- lib/riot-mongo_mapper/has_plugin.rb
+++ lib/riot-mongo_mapper/has_plugin.rb
@@ -3,7 +3,8 @@ module RiotMongoMapper
     register :has_plugin
 
     def evaluate(model, plugin)
-      if model.plugins.include? plugin
+      assert = model.respond_to?(:plugins) ? model.plugins.include?(plugin) : model.class.plugins.include?(plugin)
+      if assert
         pass("#{model} has plugin #{plugin.inspect}")
       else
         fail("expected #{model} to have plugin #{plugin.inspect}")
