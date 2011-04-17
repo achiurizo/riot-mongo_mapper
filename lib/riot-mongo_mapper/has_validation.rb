c12db324cff8128f308b5b2d2d4a90e8d2d71f7a
Lorenzo Giuliani <lorenzo@frenzart.com>
2011-04-17 03:44:59 +0200

    fix for mongo_mapper 0.9.0 now usign ActiveModel. completely stolen from
    https://github.com/thumblemonks/riot-mongoid/blob/master/lib/riot-mongoid/has_validation.rb

--- lib/riot-mongo_mapper/has_validation.rb
+++ lib/riot-mongo_mapper/has_validation.rb
@@ -3,20 +3,39 @@ module RiotMongoMapper
     register :has_validation
 
     def evaluate(model, *validation_macro_info)
-      validation_type, validation_name, options = validation_macro_info
-      validation = model.send(:all_validations).detect do |valid|
-        valid.key =~ %r{#{validation_type.to_s.camelize}} and valid.attribute == validation_name
+      validation_type, validation_field, options = validation_macro_info
+      type = validation_type.to_s
+
+      %w{validates_ _of}.each { |part| type.gsub!(%r{#{part}},'') }
+
+      validation = model._validators[validation_field].detect do |valid|
+        valid.class.name =~ /#{type.camelize}/
       end
+
       options ||= {}
+
       case
-      when validation_name.nil? || validation_type.nil?
-        fail("validation name, type and potential options must be specified with this assertion macro")
-      when validation.nil?
-        fail("expected #{model} to have validation on #{validation_name} of type #{validation_type}")
-      else
-        options_valid = options.all? { |key,value| validation.send(key) == value }
-        options_valid ? pass("#{model} has '#{validation_type}' validation '#{validation_name}' with options #{options.inspect}") :
-                        fail("expected #{model} to have validations on #{validation_name} with options #{options.inspect} of type #{validation_type}")
+        when validation_field.nil? || validation_type.nil?
+          fail("validation field, type and potential options must be specified with this assertion macro")
+
+        when validation.nil?
+          fail("expected #{model} to have validation on #{validation_field} of type #{validation_type}")
+
+        when (validation.class.name =~ %r{Length} and options[:within])
+          range = options[:within].to_a
+          if (validation.options[:minimum] == range.first) and (validation.options[:maximum] == range.last)
+            pass("#{model} has '#{validation_type}' validation '#{validation_field}' with options #{options.inspect}")
+          else
+            fail("expected #{model} to have options #{options.inspect} on validation #{validation_type}")
+          end
+
+        else
+          options_valid = options.all? { |key,value| validation.options[key] == value }
+          if options_valid
+            pass("#{model} has '#{validation_type}' validation '#{validation_field}' with options #{options.inspect}")
+          else
+            fail("expected #{model} to have options #{options.inspect} on validation #{validation_type}")
+          end
       end
     end
   end
