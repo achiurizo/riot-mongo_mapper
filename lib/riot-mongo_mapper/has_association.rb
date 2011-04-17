d15b12fef0b8108cf7e1e4373cb87d0561d5fe5b
Lorenzo Giuliani <lorenzo@frenzart.com>
2011-04-17 03:27:01 +0200

    has_association passes tests using mongo_mapper 0.9.0

--- lib/riot-mongo_mapper/has_association.rb
+++ lib/riot-mongo_mapper/has_association.rb
@@ -4,14 +4,20 @@ module RiotMongoMapper
 
     def evaluate(model, *association_macro_info)
       association_name, key_name, options = association_macro_info
-      association = model.associations[key_name.to_s]
+      association = model.associations[key_name.to_sym]
+      association_class = case association_name
+        when :many
+          MongoMapper::Plugins::Associations::ManyAssociation
+        when :one
+          MongoMapper::Plugins::Associations::OneAssociation
+        end
       if association.nil?
         fail("expected #{model} to have association #{association}")
       else
-        association_valid = association.type == association_name.to_sym
+        association_valid = association.class == association_class
         options_valid = options ? options.all? { |field,value| association.options[field] == value } : true
         options_valid && association_valid ? pass("#{model} has association '#{association_name}' on '#{key_name}' with options #{options.inspect}") :
-                                      fail("expected #{model} to have association #{association_name} with options #{options.inspect} on key #{key_name}")
+                                             fail("expected #{model} to have association #{association_name} with options #{options.inspect} on key #{key_name}")
       end
     end
   end
